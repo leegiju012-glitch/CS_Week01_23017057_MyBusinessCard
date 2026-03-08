d5c40d8cce52a764f85048043a153f2459d61f2f
gijul <gijul@>
2026-03-08 23:10:24 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace CS_Week01_23017057_MyBusinessCard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
