cdcd77a742d4dec598edaffd6f988a5490616219
기주 이 <gijulee012@gmail.com>
2026-03-08 23:21:57 +0900

    1,2단계 기능 구현

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(118, 119);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(158, 203);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("궁서체", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label1.Location = new Point(318, 198);
+            label1.Name = "label1";
+            label1.Size = new Size(134, 37);
+            label1.TabIndex = 1;
+            label1.Text = "이기주";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("맑은 고딕", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label2.Location = new Point(318, 276);
+            label2.Name = "label2";
+            label2.Size = new Size(174, 23);
+            label2.TabIndex = 2;
+            label2.Text = "컴퓨터학부 컴퓨터sw";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("맑은 고딕", 12.75F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            label3.Location = new Point(318, 299);
+            label3.Name = "label3";
+            label3.Size = new Size(184, 23);
+            label3.TabIndex = 3;
+            label3.Text = "leegiju01@naver.com";
+            label3.Click += label3_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("맑은 고딕", 15F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            button1.Location = new Point(508, 276);
+            button1.Name = "button1";
+            button1.Size = new Size(189, 46);
+            button1.TabIndex = 4;
+            button1.Text = "배경색 랜덤 변경";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Button button1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,27 @@
-namespace CS_Week01_23017057_MyBusinessCard
+﻿namespace CS_Week01_23017057_MyBusinessCard
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rd = new Random();
+            // 0~255 사이의R, G, B 값을무작위로생성하여배경색에적용
+            this.BackColor= Color.FromArgb(rd.Next(256), rd.Next(256), rd.Next(256));
         }
     }
 }
